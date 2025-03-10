e4237e6d09d2d1cf0dbc18809a402bb6eaba6925
jkof86 <cisjay@gmail.com>
2025-03-10 09:02:33 -0400

    Created register and login component files and testing menu drawer

--- app-frontend/src/index.jsx
+++ app-frontend/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
@@ -7,15 +8,17 @@ import App from './App';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: window.location.origin }}
-    >
-      <App />
-    </Auth0Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
