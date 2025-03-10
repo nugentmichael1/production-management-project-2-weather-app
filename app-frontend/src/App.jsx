9f1052583185056b64ca70e07820c63c74e33a5b
jkof86 <cisjay@gmail.com>
2025-03-10 15:13:02 -0400

    Added Responsive Navigation sidebar

--- app-frontend/src/App.jsx
+++ app-frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ResponsiveDrawer from './Components/Navigation/ResponsiveDrawer';
+import NavDrawer from './Components/Navigation/NavDrawer';
 
 export default function App() {
   return (<>
   {/* TEST */}
-    <ResponsiveDrawer />
+    <NavDrawer />
   </>)
 }
