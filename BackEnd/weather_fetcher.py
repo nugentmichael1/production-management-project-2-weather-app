6d5c53a9701fdb8b23245f0ff87bd81b39d11f3b
Michael Nugent <nugentmichael@live.com>
2025-03-12 11:13:16 -0700

    Add try-except block to handle connection error

--- BackEnd/weather_fetcher.py
+++ BackEnd/weather_fetcher.py
@@ -1,14 +1,18 @@
-import requests # For making HTTP requests
-from config import API_KEY # Import the OpenWeather API key
-import threading # For threading
-# from datetime import datetime # For debugging
-import globals# Import the global dictionary to store weather data
+import requests
+from config import API_KEY
+import threading
+import logging
+import globals
+from prometheus_client import Gauge
 
-# Prometheus metrics (if used)
-from prometheus_client import Gauge, Histogram
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
-# Define Prometheus metrics
-# Gauges are used for values that can go up and down
+# Prometheus metrics
 temperature_metric = Gauge('weather_temperature', 'Current temperature', ['city'])
 humidity_metric = Gauge('weather_humidity', 'Current humidity', ['city'])
 pressure_metric = Gauge('weather_pressure', 'Current pressure', ['city'])
@@ -45,43 +49,50 @@ weather_threads = {}
 def fetch_weather(lat:float, lon:float, stop_event)->None:
     """Fetches weather data and updates the global dictionary once per minute."""
     
-    while not stop_event.is_set(): # Check if thread should stop (for debug purposes because reloader makes duplicaite threads)
-        url = f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&lat={lat}&lon={lon}"
-        response = requests.get(url)
+    while not stop_event.is_set():
+        try:
+            url = f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&lat={lat}&lon={lon}"
+            response = requests.get(url)
 
-        if response.status_code == 200:
-            # print(f"Memory address of weather_data in {__name__}: {id(weather_data)}")  # Debug log
-            data = response.json()
-            weather_data = {
-                "city": data["name"],
-                "temperature": data["main"]["temp"],
-                "temperature_feels_like": data["main"]["feels_like"],
-                "humidity": data["main"]["humidity"],
-                "pressure": data["main"]["pressure"],
-                "weather": data["weather"][0]["description"],
-                "wind_speed": data["wind"]["speed"],
-                "wind_direction": data["wind"]["deg"],
-                "wind_gust": data["wind"].get("gust",0),
-                "cloudiness": data["clouds"]["all"],
-            }
-            
-            # save_weather_data(weather_data) # Save the weather data to a file (this is our psuedo database)
-            with globals.weather_data_lock:
-                globals.all_weather_data[weather_data["city"]] = weather_data # Update the global dictionary with the new weather data
+            if response.status_code == 200:
+                # print(f"Memory address of weather_data in {__name__}: {id(weather_data)}")  # Debug log
+                data = response.json()
+                weather_data = {
+                    "city": data["name"],
+                    "temperature": data["main"]["temp"],
+                    "temperature_feels_like": data["main"]["feels_like"],
+                    "humidity": data["main"]["humidity"],
+                    "pressure": data["main"]["pressure"],
+                    "weather": data["weather"][0]["description"],
+                    "wind_speed": data["wind"]["speed"],
+                    "wind_direction": data["wind"]["deg"],
+                    "wind_gust": data["wind"].get("gust",0),
+                    "cloudiness": data["clouds"]["all"],
+                }
+                
+                # save_weather_data(weather_data) # Save the weather data to a file (this is our psuedo database)
+                with globals.weather_data_lock:
+                    globals.all_weather_data[weather_data["city"]] = weather_data # Update the global dictionary with the new weather data
 
-            # Update Prometheus metrics
-            temperature_metric.labels(city=weather_data["city"]).set(weather_data["temperature"])
-            humidity_metric.labels(city=weather_data["city"]).set(weather_data["humidity"])
-            pressure_metric.labels(city=weather_data["city"]).set(weather_data["pressure"])
-            wind_speed_metric.labels(city=weather_data["city"]).set(weather_data["wind_speed"])
-            wind_direction_metric.labels(city=weather_data["city"]).set(weather_data["wind_direction"])
-            wind_gust_metric.labels(city=weather_data["city"]).set(weather_data["wind_gust"])
-            cloudiness_metric.labels(city=weather_data["city"]).set(weather_data["cloudiness"])
-            
-            # print(f"Updated weather data: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {weather_data}")  # Debug log
+                # Update Prometheus metrics
+                temperature_metric.labels(city=weather_data["city"]).set(weather_data["temperature"])
+                humidity_metric.labels(city=weather_data["city"]).set(weather_data["humidity"])
+                pressure_metric.labels(city=weather_data["city"]).set(weather_data["pressure"])
+                wind_speed_metric.labels(city=weather_data["city"]).set(weather_data["wind_speed"])
+                wind_direction_metric.labels(city=weather_data["city"]).set(weather_data["wind_direction"])
+                wind_gust_metric.labels(city=weather_data["city"]).set(weather_data["wind_gust"])
+                cloudiness_metric.labels(city=weather_data["city"]).set(weather_data["cloudiness"])
+                
+                logging.info(f"Weather data updated for {weather_data['city']}")
 
-        else:
-            print(f"Failed to fetch weather data: {response.status_code}")
+            else:
+                logging.warning(f"Failed to fetch weather data: {response.status_code} for lat {lat}, lon {lon}")
+                
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Weather fetch error: {e}")
+        
+        except Exception as e:
+            logging.critical(f"Unexpected error in weather thread: {e}", exc_info=True)
 
         stop_event.wait(60)  # Wait for 1 minute or until the event is set
 
@@ -95,20 +106,19 @@ def start_weather_threads():
     # Stop any existing threads
     stop_all_weather_threads()
     
-    # Start new threads
     for city, coords in CITIES.items():
-        # print(f"Starting weather thread for {city}...")
-        # print(f"coords:{coords}")
-        # print(f"Coordinates: lat:{coords["lat"]}, lon:{coords["lon"]}")
         stop_events[city] = threading.Event() # Create a stop event for each city
         weather_threads[city] = threading.Thread(target=fetch_weather, args=(coords["lat"], coords["lon"], stop_events[city]), daemon=True)
         weather_threads[city].start()
+        logging.info(f"Started weather thread for {city}")
     
 def stop_all_weather_threads():
     """Stops all weather threads."""
     for city, thread in weather_threads.items():
-        print(f"Stopping weather thread for {city}...")
+        logging.info(f"Stopping weather thread for {city}")
         stop_events[city].set() # Set the event to stop the thread
         thread.join() # Wait for the thread to finish
+        
     weather_threads.clear() # Clear the dictionary
     stop_events.clear() # Clear the dictionary
+    logging.info("All weather threads stopped.")
